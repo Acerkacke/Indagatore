645c1a069176bfe39b6bd44aa718816d4d0d56e7
Acerkacke <acerkacke@gmail.com>
2016-02-12 21:13:02 +0100

    Third Commit
    Terzo commit con integrata collisione con oggetti statici

---
+++ Assets/Script/MatricePosizioni.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MatricePosizioni : MonoBehaviour {
+
+	public GameObject[,] matrice;
+
+	void Start () {
+		matrice = new GameObject[20,20];
+		AutoCheck();
+	}
+
+	void AutoCheck(){
+		GameObject[] autoGameObjects = GameObject.FindGameObjectsWithTag("Obj");
+		foreach(GameObject go in autoGameObjects){
+			int posx = (int)(go.transform.position.x - 0.5f + 10);
+			int posz = (int)(go.transform.position.z - 0.5f + 10);
+			Debug.Log("AutoCheck - Trovato oggetto in pos " + posx + "," + posz);
+			matrice[posx,posz] = go;
+		}
+	}
+
+	// Update is called once per frame
+	void Update () {
+	
+	}
+	/// <summary>
+	/// Controlla se la posizione indicata e' libera o meno.
+	/// </summary>
+	/// <returns><c>true</c>, se libero, <c>false</c> se occupato da qualcosa.</returns>
+	/// <param name="posx">posx</param>
+	/// <param name="posy">posy</param>
+	public bool eLibero(int posx, int posy){
+		if(matrice[posx,posy] != null)
+			return false;
+		return true;
+	}
+
+	/// <summary>
+	/// Converte l'asse a position in matrice.
+	/// </summary>
+	/// <returns>La posizione nella matrice.</returns>
+	/// <param name="asse">Asse.</param>
+	public static int convertiAsseAPos(float asse){
+		return (int)(asse - 0.5f + 10);
+	}
+}
---
+++ Assets/Script/PlayerScript.cs
@@ -0,0 +1,65 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class PlayerScript : MonoBehaviour {
+
+	private float ogniQuantoSposta = 0.20f;
+	private float tempoSpostamento = 0.05f;
+	private float timer = 0;
+	private Vector3 targetPos;
+	private List<Vector3> lastPos;
+	private MatricePosizioni matricePos;
+
+	void Start () {
+		targetPos = transform.position;
+		lastPos = new List<Vector3>();
+		matricePos = GameObject.FindObjectOfType<MatricePosizioni>();
+	}
+
+
+
+	void Update () {
+		Trasla();
+		if(timer >= 0){
+			timer -= Time.deltaTime;
+		}
+		if(timer <= 0 ){
+			if(Input.GetKeyDown(KeyCode.RightArrow)){
+				setTargetPos(new Vector3(targetPos.x+1,targetPos.y,targetPos.z));
+			}
+			if(Input.GetKeyDown(KeyCode.LeftArrow)){
+				setTargetPos(new Vector3(targetPos.x-1,targetPos.y,targetPos.z));
+			}
+			if(Input.GetKeyDown(KeyCode.UpArrow)){
+				setTargetPos(new Vector3(targetPos.x,targetPos.y,targetPos.z+1));
+			}
+			if(Input.GetKeyDown(KeyCode.DownArrow)){
+				setTargetPos(new Vector3(targetPos.x,targetPos.y,targetPos.z-1));
+			}
+		}
+	}
+
+	void setTargetPos(Vector3 pos){
+		if(canIGoThere(pos)){
+			targetPos = pos;
+			timer = ogniQuantoSposta;
+			lastPos.Add(targetPos);
+		}else{
+			Debug.Log("setTargetPos - Non puoi andare dove vuoi andare, c'è qualcosa in mezzo");
+		}
+	}
+
+	bool canIGoThere(Vector3 pos){
+		int posx = MatricePosizioni.convertiAsseAPos(pos.x);
+		int posz = MatricePosizioni.convertiAsseAPos(pos.z);
+		return matricePos.eLibero(posx,posz);
+	}
+
+	void Trasla(){
+		if(transform.position != targetPos){
+			Vector3 velocity = Vector3.zero;
+			transform.position = Vector3.SmoothDamp(transform.position,targetPos,ref velocity,tempoSpostamento);
+		}
+	}
+}
