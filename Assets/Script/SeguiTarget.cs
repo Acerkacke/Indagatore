0c01695bef3659d2ddd14aee6cc6819396adbc9e
Acerkacke <acerkacke@gmail.com>
2016-02-12 22:17:11 +0100

    Aggiustamenti
    piccoli aggiustamenti

--- Assets/Script/SeguiTarget.cs
+++ Assets/Script/SeguiTarget.cs
@@ -9,8 +9,8 @@ public class SeguiTarget : MonoBehaviour {
 	void Update () {
 		if(target != null){
 			Vector3 velocity = Vector3.zero;
-			Vector3 forward = target.forward * 8.0f;
-			Vector3 up = target.up * 7.0f;
+			Vector3 forward = target.forward * 4.5f;
+			Vector3 up = target.up * 5.0f;
 			Vector3 targetPos = target.position - forward + up;
 			transform.position = Vector3.SmoothDamp(transform.position, targetPos, ref velocity,tempo);
 			transform.LookAt (target);
